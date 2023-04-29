ee24c8940c0455624b5e87f4731e4b6730477859
Ayush <kediaayush26@gmail.com>
2023-04-29 12:38:39 +0530

    bl hack

---
+++ frontend/src/components/Home/HeroSection.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import bgpic from './../../images/health.jpg'
+import { Button } from './Button';
+import './HeroSection.css';
+
+function HeroSection() {
+
+  const myStyle={
+    backgroundImage: `url(${bgpic})`,
+    height:'100vh',
+    marginTop:'-70px',
+    fontSize:'50px',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+};
+  return (
+    <div className='hero-container' style={myStyle}>
+      {/* <video src='/videos/video-1.mp4' autoPlay loop muted /> */}
+      <h1>Become healthy NOW</h1>
+      <p>What are you waiting for?</p>
+      <div className='hero-btns'>
+        <Button
+          className='btns'
+          buttonStyle='btn--outline'
+          buttonSize='btn--large'
+        >
+         get started
+        </Button>
+
+       
+      </div>
+    </div>
+  );
+}
+
+export default HeroSection;
